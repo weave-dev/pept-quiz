13aebeb5147c79d07c76c7aff6432020a6244ede
weave.dev <weave.dev1@gmail.com>
2025-05-06 23:01:22 +0800

    WIP: dummy components page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import Button from "$lib/components/base/Button.svelte";
+</script>
+
+<h1 class="font-serif text-6xl">Fonts</h1>
+<h1 class="font-serif text-6xl font-light">Welcome to SvelteKit</h1>
+<h1 class="font-serif font-regular text-6xl">Welcome to SvelteKit</h1>
+<h1 class="font-serif text-6xl font-medium">Welcome to SvelteKit</h1>
+<h1 class="font-serif text-6xl font-semibold">Welcome to SvelteKit</h1>
+<h1 class="font-serif text-6xl font-bold">Welcome to SvelteKit</h1>
+
+<h1 class="text-6xl font-light">Welcome to SvelteKit</h1>
+<h1 class="font-regular text-6xl">Welcome to SvelteKit</h1>
+<h1 class="text-6xl font-medium">Welcome to SvelteKit</h1>
+<h1 class="text-6xl font-semibold">Welcome to SvelteKit</h1>
+<h1 class="text-6xl font-bold">Welcome to SvelteKit</h1>
+
+<h1 class="font-serif text-4xl font-bold p-2">Lorem, ipsum dolor.</h1>
+<Button>Click Me</Button>
+
+<p class="font-serif p-2">
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure architecto, illum velit assumenda
+	eveniet quasi aspernatur unde ratione voluptas vel inventore eum, provident totam soluta, fugiat
+	amet. Temporibus deleniti, autem repellendus quidem enim ab! Fugit culpa dolorem voluptatibus
+	voluptas et neque ut iusto consequatur ad nisi hic suscipit placeat corrupti magni adipisci
+	quibusdam cupiditate obcaecati, autem quod rerum ullam nobis dolorum. Doloribus illum eum soluta
+	beatae molestias doloremque repudiandae, aspernatur omnis laborum minus consequuntur optio vitae
+	dicta quisquam cumque deserunt consectetur. Enim beatae explicabo iure. Officia provident
+	excepturi consequatur deserunt molestias exercitationem, dicta aperiam vel debitis in qui totam
+	unde quidem, fuga consequuntur praesentium, corrupti adipisci veritatis corporis facere quo!
+	Recusandae molestias distinctio explicabo corporis nesciunt placeat soluta odit optio ex sint
+	ipsa, nostrum, obcaecati aliquam qui cum repellat sapiente voluptate eum quibusdam ratione numquam
+	excepturi? Qui dolorum voluptate veniam quam libero ullam architecto enim fugit. Architecto optio
+	illum ad debitis labore placeat voluptas possimus quibusdam quis, maxime dolore. Eligendi dolorem
+	quas nihil, hic distinctio ipsum. Fugiat in alias exercitationem corporis? Repellat totam neque
+	accusantium! Temporibus, molestiae. Tempora veniam expedita officia! Veniam dolores voluptates
+	consectetur libero velit praesentium. Aperiam deleniti a magni, architecto reiciendis impedit
+	reprehenderit tempora dolorem vel quo.
+</p>
