857c818d92fb87107e166f7d3c1389616ea2e92d
weave.dev <weave.dev1@gmail.com>
2025-05-12 21:32:02 +0800

    chore: updated ui of button component

--- src/lib/components/base/Button.svelte
+++ src/lib/components/base/Button.svelte
@@ -1,9 +1,60 @@
-<script>
-	const props = $props();
+<script lang="ts">
+	import { BgColors } from '$lib/types';
+	import type { BgColorsValues, TextColorsValues } from '$lib/types';
+	import type { Snippet } from 'svelte';
+	import { twMerge } from 'tailwind-merge';
 
-	const btnClass = `rounded-xl px-5 py-2 bg-gray-700 text-white shadow`
+	interface Props {
+		variant: BgColorsValues;
+		children: Snippet;
+		class?: string;
+	}
+
+	const {
+		children,
+		variant = BgColors.NEUTRAL,
+		class: customClass,
+		...rest
+	}: Props = $props();
+
+	const bgColor = $derived(
+		!variant.includes('dark')
+			? variant.includes('gray')
+				? 'bg-gray-700'
+				: `${variant}-darker`
+			: `${variant}-darker`
+	);
+
+	const btnClass = $derived(
+		twMerge([
+			'rounded-lg hover:[&>span]:-translate-y-1.5 active:[&>span]:translate-y-0 cursor-pointer select-none',
+			bgColor,
+			customClass
+		])
+	);
+
+	const frontBorder = $derived(['border-2', bgColor.replace('bg', 'border')]);
+	const txtColor = $derived(
+		bgColor.replace('bg', 'text').replace('-darker', '').replace('-dark', '')
+	);
+
+	const textColor = $derived([
+		txtColor.includes('gray') ? txtColor : txtColor + '-content'
+	]);
+
+	const frontClass = $derived(
+		twMerge([
+			`transition-all duration-75 block -translate-y-2 rounded-lg px-8 py-2`,
+			frontBorder,
+			textColor,
+			customClass,
+			variant
+		])
+	);
 </script>
 
-<button class={btnClass} {...props}>
-	{@render props.children()}
+<button class={btnClass} {...rest}>
+	<span class={frontClass}>
+		{@render children()}
+	</span>
 </button>
