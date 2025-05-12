70fe0193f193b78b63e2617a6f6e0e6bcb79b8cb
weave.dev <weave.dev1@gmail.com>
2025-05-12 21:32:52 +0800

    WIP: added sandbox pages

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -2,7 +2,7 @@
 	import Button from '$lib/components/base/Button.svelte';
 </script>
 
-<h1 class="text-9xl text-violet-800">Hello! 👋</h1>
+<h1 class="text-9xl text-bittersweet-red">Hello! 👋</h1>
 <a href="/">Go back home</a>
 
 {#each { length: 100 }}
