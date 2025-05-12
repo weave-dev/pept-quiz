e67345dc0eef097c2ed920f0075b50f5992d18aa
weave.dev <weave.dev1@gmail.com>
2025-05-12 18:38:01 +0800

    chore: changed routing strategy to hash

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,15 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({
+			fallback: '200.html' // may differ from host to host
+		}),
+
+		router: {
+			type: 'hash'
+		}
+	}
 };
 
 export default config;
