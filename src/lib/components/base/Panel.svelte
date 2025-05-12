da13d0df128f2228af796533c62ea5ae748d32e7
weave.dev <weave.dev1@gmail.com>
2025-05-12 18:37:45 +0800

    chore: created panel component

---
+++ src/lib/components/base/Panel.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { Variants, type VariantsValues } from '$lib/types';
+	import type { Snippet } from 'svelte';
+	import { twMerge } from 'tailwind-merge';
+
+	interface Props {
+		children: Snippet;
+		class?: string;
+		variant?: VariantsValues;
+	}
+
+	let {
+		children,
+		class: customClass,
+		variant = Variants.NEUTRAL
+	}: Props = $props();
+
+	let variantClass = $derived([`border-${variant}`, `shadow-${variant}`]);
+
+	let mergedClass = $derived(
+		twMerge(['rounded-lg border-2 p-4', variantClass, customClass])
+	);
+</script>
+
+<div class={mergedClass}>
+	{@render children?.()}
+</div>
