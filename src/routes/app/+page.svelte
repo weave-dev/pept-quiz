a77680788021071a95f4638eb21060ffecfc79c8
weave.dev <weave.dev1@gmail.com>
2025-05-13 09:10:55 +0800

    WIP: minor changes in app page

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -1,10 +1,16 @@
 <script>
 	import Button from '$lib/components/base/Button.svelte';
+	import Panel from '$lib/components/base/Panel.svelte';
+	import { BgColors } from '$lib/types';
 </script>
 
-<h1 class="text-9xl text-bittersweet-red">Hello! 👋</h1>
-<a href="/">Go back home</a>
-
-{#each { length: 100 }}
-	<Button>Click Me!</Button>
-{/each}
+<div class="m-4">
+	<Panel>
+		<Button variant={BgColors.TEXAS_YELLOW}>Click Me</Button>
+		<Button variant={BgColors.FERN_GREEN}>Click Me</Button>
+		<Button variant={BgColors.BITTERSWEET_RED}>Click Me</Button>
+		<Button variant={BgColors.TEXAS_YELLOW}>Click Me</Button>
+		<Button variant={BgColors.PICTON_BLUE}>Click Me</Button>
+		<Button variant={BgColors.AMETHYST_PURPLE}>Click Me</Button>
+	</Panel>
+</div>
