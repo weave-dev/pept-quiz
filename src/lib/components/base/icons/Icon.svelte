1c96d8a09c3d9106ab7782ae48a7c4b231c9de55
weave.dev <weave.dev1@gmail.com>
2025-05-23 01:19:25 +0800

    chore: added custom icon component

--- src/lib/components/base/icons/Icon.svelte
+++ src/lib/components/base/icons/Icon.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
 	import { icons } from '.';
+
+	type Props = {
+		icon: keyof typeof icons;
+	};
+
+	const { icon, ...rest }: Props = $props();
+	const IconComponent = $derived(icons[icon]);
 </script>
 
-<icons.abacus />
+<IconComponent {...rest} />
