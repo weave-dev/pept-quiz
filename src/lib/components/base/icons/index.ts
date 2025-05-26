77598970a0cf332eb938034d5fd1a923b31c860a
weave.dev <weave.dev1@gmail.com>
2025-05-26 23:15:45 +0800

    chore: added lock icon

--- src/lib/components/base/icons/index.ts
+++ src/lib/components/base/icons/index.ts
@@ -1,11 +1,13 @@
-import FluentEmojiFlatAbacus from '~icons/fluent-emoji-flat/abacus';
-import FluentEmojiFlatInputLatinLetters from '~icons/fluent-emoji-flat/input-latin-letters';
-import FluentStar12Filled from '~icons/fluent/star-12-filled';
+import FluentEmojiFlatAbacus from '~icons/fluent-emoji-flat/abacus'
+import FluentEmojiFlatInputLatinLetters from '~icons/fluent-emoji-flat/input-latin-letters'
+import FluentStar12Filled from '~icons/fluent/star-12-filled'
+import FluentLockClosed16Filled from '~icons/fluent/lock-closed-16-filled'
 
-export type UnpluginIconName = keyof typeof icons;
+export type UnpluginIconName = keyof typeof icons
 
 export const icons = {
 	abacus: FluentEmojiFlatAbacus,
 	inputLatinLetters: FluentEmojiFlatInputLatinLetters,
-	star12Filled: FluentStar12Filled
-};
+	star12Filled: FluentStar12Filled,
+	lockClosed16Filled: FluentLockClosed16Filled
+}
