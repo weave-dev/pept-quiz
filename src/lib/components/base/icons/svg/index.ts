c2b6ef0790d27cb6bbf759498df1f70b0ac96f14
weave.dev <weave.dev1@gmail.com>
2025-06-06 01:27:36 +0800

    feat: added SvgIcon component, and test svg icons

---
+++ src/lib/components/base/icons/svg/index.ts
@@ -0,0 +1,9 @@
+import Char1 from '$lib/svg-icons/char-1.svg'
+import Char2 from '$lib/svg-icons/char-2.svg'
+
+export type SvgIconName = keyof typeof icons
+
+export const icons = {
+	Char1,
+	Char2
+}
