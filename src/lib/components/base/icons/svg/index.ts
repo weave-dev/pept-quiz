1c3e216c09b084adfed0d3704e47623929da4651
weave.dev <weave.dev1@gmail.com>
2025-06-11 21:19:34 +0800

    chore: added chicken to svg types

--- src/lib/components/base/icons/svg/index.ts
+++ src/lib/components/base/icons/svg/index.ts
@@ -1,9 +1,11 @@
 import Char1 from '$lib/svg-icons/char-1.svg'
 import Char2 from '$lib/svg-icons/char-2.svg'
+import Chicken from '$lib/svg-icons/chicken.svg'
 
 export type SvgIconName = keyof typeof icons
 
 export const icons = {
 	Char1,
-	Char2
+	Char2,
+	Chicken
 }
